31b209399f7f1a71e37bdf73074a9db24373f948
nadeemshariff <108820002+nadeemshariff@users.noreply.github.com>
2024-06-04 01:16:32 +0530

    tw

--- tailwind-site/tailwind.config.js
+++ tailwind-site/tailwind.config.js
@@ -2,10 +2,14 @@
 export default {
   content: [
     "./index.html",
-    // "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      color:{
+        
+      }
+    },
   },
   plugins: [],
 }
